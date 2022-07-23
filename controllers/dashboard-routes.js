edb2e44ce793f75376f14072b362a8a719805df1
ommarkhawaja1 <ommarkhawaja1@gmail.com>
2022-07-23 11:36:47 -0400

    working server

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,53 +1,85 @@
 const router = require('express').Router();
-const { Post } = require('../models/');
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-  try {
-    // store the results of the db query in a variable called postData. should use something that "finds all" from the Post model. may need a where clause!
-    const postData = 
-    // this sanitizes the data we just got from the db above (you have to create the above)
-    const posts = postData.map((post) => post.get({ plain: true }));
-
-    // fill in the view to be rendered
-    res.render('hmmmm what goes here', {
-      // this is how we specify a different layout other than main! no change needed
-      layout: 'dashboard',
-      // coming from line 10 above, no change needed
-      posts,
+router.get('/', withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'content',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      // serialize data before passing to template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
-  } catch (err) {
-    res.redirect('login');
-  }
 });
 
-router.get('/new', withAuth, (req, res) => {
-  // what view should we send the client when they want to create a new-post? (change this next line)
-  res.render('hmmmm what goes here', {
-    // again, rendering with a different layout than main! no change needed
-    layout: 'dashboard',
-  });
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+
+        res.render('edit-post', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
-router.get('/edit/:id', withAuth, async (req, res) => {
-  try {
-    // what should we pass here? we need to get some data passed via the request body
-    const postData = await Post.findByPk(????);
-
-    if (postData) {
-      // serializing the data
-      const post = postData.get({ plain: true });
-      // which view should we render if we want to edit a post?
-      res.render('hmmmm what goes here', {
-        layout: 'dashboard',
-        post,
-      });
-    } else {
-      res.status(404).end();
-    }
-  } catch (err) {
-    res.redirect('login');
-  }
-});
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,52 +1,44 @@
 const router = require('express').Router();
-const { Post, Comment, User } = require('../models/');
+const sequelize = require('../config/connection');
+const { Post, User, Comment} = require('../models');
 
-// get all posts for homepage
-router.get('/', async (req, res) => {
-  try {
-    // we need to get all Posts and include the User for each (change lines 8 and 9)
-    const postData = await SomeModel.someSequelizeMethod({
-      include: [SomeOtherModel],
+router.get('/', (req, res) => {
+  console.log(req.session)
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }))
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
-    // serialize the data
-    const posts = postData.map((post) => post.get({ plain: true }));
-    // we should render all the posts here
-    res.render('hmmmm what view should we render?', { posts });
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
-// get single post
-router.get('/post/:id', async (req, res) => {
-  try {
-    // what should we pass here? we need to get some data passed via the request body (something.something.id?)
-    // change the model below, but not the findByPk method.
-    const postData = await SomeModel.findByPk(????, {
-      // helping you out with the include here, no changes necessary
-      include: [
-        User,
-        {
-          model: Comment,
-          include: [User],
-        },
-      ],
-    });
-
-    if (postData) {
-      // serialize the data
-      const post = postData.get({ plain: true });
-      // which view should we render for a single-post?
-      res.render('hmmmm what view should we render?', { post });
-    } else {
-      res.status(404).end();
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// giving you the login and signup route pieces below, no changes needed.
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
@@ -56,13 +48,52 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/signup', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
 
-  res.render('signup');
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render('single-post', {
+        post,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
 
 module.exports = router;
